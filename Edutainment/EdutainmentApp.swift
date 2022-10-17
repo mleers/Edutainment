caf778b7c71b5ba5a337d2991372b3047215c5d8
mleers <mleers4@gmail.com>
2022-10-17 15:10:20 -0700

    Initial Commit

---
+++ Edutainment/EdutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdutainmentApp.swift
+//  Edutainment
+//
+//  Created by Mitch on 10/17/22.
+//
+
+import SwiftUI
+
+@main
+struct EdutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
